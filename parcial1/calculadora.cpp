911f1dd552411373217d2f2843959ae50ea13f64
BernaRM111 <124811292+BernaRM111@users.noreply.github.com>
2023-02-14 21:35:40 +0000

    add hello.cpp

---
+++ parcial1/calculadora.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "hello wordl" << endl;
+	cout << "hello galaxy" << endl;
+  	cout << "hello universe" << endl;
+
+	return 0;
+} 
